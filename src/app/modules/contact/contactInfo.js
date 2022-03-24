b75dec47f3e78d74e72890c72ab75640aad0eabc
Ariel <>
2022-03-24 14:29:52 +0800

    1. firebase設定更新 2. 聯絡我們加入表格結構

--- src/app/modules/contact/contactInfo.js
+++ src/app/modules/contact/contactInfo.js
@@ -1,7 +1,81 @@
 import React from 'react'
+import { Form, Input, InputNumber, Button } from 'antd';
 
-export default () => (
-    <div>
-        <h1>聯絡我們</h1>    
-    </div>
-)
+const layout = {
+    labelCol: { span: 4 },
+    wrapperCol: { span: 8 },
+};
+
+/* eslint-disable no-template-curly-in-string */
+const validateMessages = {
+    required: '${label} is required!',
+    types: {
+        email: '${label} is not a valid email!',
+        number: '${label} is not a valid number!',
+    },
+    number: {
+        range: '${label} must be between ${min} and ${max}',
+    },
+};
+/* eslint-enable no-template-curly-in-string */
+
+const contact = props => { 
+    const onFinish = (values) => {
+        console.log(values);
+    };
+    return (
+        <div class="contact">
+            <div class="contactTitle">
+                聯絡我們
+            </div>
+            <div class="contactForm">
+                <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
+                    <Form.Item
+                        name={['user', 'name']}
+                        label="Name"
+                        rules={[
+                            { required: true },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name={['user', 'email']}
+                        label="Email"
+                        rules={[
+                            { type: 'email' },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name={['user', 'age']}
+                        label="Age"
+                        rules={[
+                            {
+                            type: 'number',
+                            min: 0,
+                            max: 99,
+                            },
+                        ]}
+                    >
+                        <InputNumber />
+                    </Form.Item>
+                    <Form.Item name={['user', 'website']} label="Website">
+                        <Input />
+                    </Form.Item>
+                    <Form.Item name={['user', 'introduction']} label="Introduction">
+                        <Input.TextArea />
+                    </Form.Item>
+                    <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        </div>
+    );
+};
+
+export default contact
