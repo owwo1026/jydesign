01f786f7caa1ec8d77a58e8e55fd911d842d8881
Ariel <>
2022-03-29 18:37:48 +0800

    1. 2. 3.

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-// import logo from '../logo.svg';
+import united_design_headerImg from "./assets/images/img/test01.jpeg"
 import './App.css';
 import './assets/css/main.css';
 import Routes from './Routes';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
-import { Layout, Menu, Breadcrumb } from 'antd';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import { Layout } from 'antd';
 
 // const { Header, Content, Footer } = Layout;
 export default function App() {
--- src/app/components/header/header.js
+++ src/app/components/header/header.js
@@ -4,25 +4,26 @@ import united_design_logo from "../../assets/images/united_design_logo.png"
 import united_design_headerImg from "../../assets/images/img/test01.jpeg"
 
 export default () => (
-    <Affix offsetTop={1} onChange={(affixed) => console.log(affixed)}>
         <header id='header' class='header'>
-            <div class="logo">
-                <a role="button" href="/">
-                    <img src={united_design_logo} alt="凝聚室內設計"/>
-                </a>
-            </div>
-            <nav class='menu'>
-                <ul class="navbar-nav">
-                    <li class="nav-item"><a class="nav-link" role="button" href="/about">關於我們</a></li>
-                    <li class="nav-item"><a class="nav-link" role="button" href="/works">作品集</a></li>
-                    <li class="nav-item"><a class="nav-link" role="button" href="/sop">流程介紹</a></li>
-                    <li class="nav-item"><a class="nav-link" role="button" href="/faq">常見問題</a></li>
-                    <li class="nav-item"><a class="nav-link" role="button" href="/contact">聯絡我們</a></li>
-                </ul>
-            </nav>
+            {/* <Affix offsetTop={1}> */}
+                {/* style = {{backgroundColor: 'tomato'}} */}
+                <div class="logo">
+                    <a role="button" href="/">
+                        <img style={{width: '70%'}} src={united_design_logo} alt="凝聚室內設計"/>
+                    </a>
+                </div>
+                <nav class='menu'>
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a class="nav-link" role="button" href="/about">關於我們</a></li>
+                        <li class="nav-item"><a class="nav-link" role="button" href="/works">作品集</a></li>
+                        <li class="nav-item"><a class="nav-link" role="button" href="/sop">流程介紹</a></li>
+                        <li class="nav-item"><a class="nav-link" role="button" href="/faq">常見問題</a></li>
+                        <li class="nav-item"><a class="nav-link" role="button" href="/contact">聯絡我們</a></li>
+                    </ul>
+                </nav>
+            {/* </Affix> */}
             <div class = 'headerBottom'>
-                {/* <img style={{width: '70%'}} src={united_design_headerImg} alt="凝聚室內設計"/> */}
+                <img style={{width: '100%'}} src={united_design_headerImg} alt="凝聚室內設計"/>
             </div>
         </header>
-    </Affix>
 )
